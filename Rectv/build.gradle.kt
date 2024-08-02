786251787641ce64f5f2fac338a2b787740c588b
Amiqo09 <106442600+Amiqo09@users.noreply.github.com>
2024-08-02 20:54:21 +0400

    update Rectv/build.gradle.kt

---
+++ Rectv/build.gradle.kt
@@ -0,0 +1,18 @@
+version = 9
+
+cloudstream {
+    authors     = listOf("yusiqo")
+    language    = "tr"
+    description = "Üşenmeden Rectv Den Canlı olarak film dizi çeken eklenti, lordun elinden"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie","TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=diziyou.co&sz=%size%"
+}
