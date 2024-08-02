0beb2ffbe2cd0132e092f6cc863cd7a5b3fbe532
Amiqo09 <106442600+Amiqo09@users.noreply.github.com>
2024-08-02 23:03:26 +0400

    update Rectv/src/main/kotlin/com/yusiqo/RectvModels.kt

--- Rectv/src/main/kotlin/com/yusiqo/RectvModels.kt
+++ Rectv/src/main/kotlin/com/yusiqo/RectvModels.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 
 data class SearchItem(
-val title: String?,
+    val title: String?,
     val id: String?,
     val image: String?,
     val trTitle: String?,
@@ -16,5 +16,40 @@ val title: String?,
     val year: String?,
     val view: Int?,
     val type: String = "defaultType",
+    val url: String?,
+    val label: String?,
+    val sublabel: String?,
+    val description: String?,
+    val comment: Boolean?,
+    val rating: Int?,
+    val downloadas: String?,
+    val playas: String?,
+    val classification: String?,
+    val cover: String?,
+    val genres: List<Genre>?,
+    val trailer: Trailer?,
+    val sources: List<Source>?
+)
+
+data class Genre(
+    val id: Int?,
+    val title: String?
+)
+
+data class Trailer(
+    val id: Int?,
+    val type: String?,
+    val url: String?
+)
+
+data class Source(
+    val id: Int?,
+    val title: String?,
+    val quality: String?,
+    val size: String?,
+    val kind: String?,
+    val premium: String?,
+    val external: Boolean?,
+    val type: String?,
     val url: String?
 )
