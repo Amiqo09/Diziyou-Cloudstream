48183aaf87f8f7b48bef7c6594e5784a483b42c0
Amiqo09 <106442600+Amiqo09@users.noreply.github.com>
2024-08-02 21:20:06 +0400

    update Rectv/build.gradle.kts

--- Rectv/build.gradle.kts
+++ Rectv/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie","TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=rectvapk.cc&sz=%size%"
+    iconUrl = "https://rectvapk.cc/favicon.ico"
 }
