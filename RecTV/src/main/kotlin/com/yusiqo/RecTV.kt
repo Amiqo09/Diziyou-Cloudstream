da76ce2818238415b0877f6bf07cab66c02dd791
Amiqo09 <106442600+Amiqo09@users.noreply.github.com>
2024-08-03 16:13:10 +0400

    update RecTV/src/main/kotlin/com/yusiqo/RecTV.kt

--- RecTV/src/main/kotlin/com/yusiqo/RecTV.kt
+++ RecTV/src/main/kotlin/com/yusiqo/RecTV.kt
@@ -1,11 +1,11 @@
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+// ! https://github.com/Amiqo09/Diziyou-Cloudstream
 
 package com.yusiqo
 
 import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 class RecTV : MainAPI() {
@@ -16,21 +16,29 @@ class RecTV : MainAPI() {
     override val hasQuickSearch       = false
     override val hasChromecastSupport = true
     override val hasDownloadSupport   = true
-    override val supportedTypes       = setOf(TvType.Movie)
+    override val supportedTypes       = setOf(TvType.Movie, TvType.Live)
 
     override val mainPage = mainPageOf(
-        "${mainUrl}/api/movie/by/filtres/26/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/" to "Türkçe Dublaj Filmler",
-        "${mainUrl}/api/movie/by/filtres/1/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Aksiyon Filmleri",
-        "${mainUrl}/api/movie/by/filtres/2/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Dram Filmleri",
-        "${mainUrl}/api/movie/by/filtres/3/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Komedi Filmler",
-        "${mainUrl}/api/movie/by/filtres/4/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Bilim Kurgu Filmler",
-        "${mainUrl}/api/movie/by/filtres/5/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Romantik Filmler",
-        "${mainUrl}/api/movie/by/filtres/8/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Korku Filmleri",
-        "${mainUrl}/api/movie/by/filtres/0/created/0/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Son Yüklenen Filmler",
+        "${mainUrl}/api/channel/by/filtres/0/0/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"      to "Canlı",
+        "${mainUrl}/api/movie/by/filtres/0/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Son Yüklenen",
+        "${mainUrl}/api/movie/by/filtres/14/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/" to "Aile",
+        "${mainUrl}/api/movie/by/filtres/1/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Aksiyon",
+        "${mainUrl}/api/movie/by/filtres/13/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/" to "Animasyon",
+        "${mainUrl}/api/movie/by/filtres/19/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/" to "Belgesel",
+        "${mainUrl}/api/movie/by/filtres/4/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Bilim Kurgu",
+        "${mainUrl}/api/movie/by/filtres/2/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Dram",
+        "${mainUrl}/api/movie/by/filtres/10/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/" to "Fantastik",
+        "${mainUrl}/api/movie/by/filtres/3/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Komedi",
+        "${mainUrl}/api/movie/by/filtres/8/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Korku",
+        "${mainUrl}/api/movie/by/filtres/17/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/" to "Macera",
+        "${mainUrl}/api/movie/by/filtres/5/created/SAYFA/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/"  to "Romantik"
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val home   = app.get("${request.data}")
+        val page = page - 1
+
+        val url  = request.data.replace("SAYFA", "${page}")
+        val home = app.get(url)
 
         val movies = AppUtils.tryParseJson<List<RecItem>>(home.text)!!.mapNotNull { item ->
             val toDict = jacksonObjectMapper().writeValueAsString(item)
@@ -42,7 +50,24 @@ class RecTV : MainAPI() {
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf<SearchResponse>()
+        val home    = app.get("${mainUrl}/api/search/${query}/4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/")
+        val veriler = AppUtils.tryParseJson<RecSearch>(home.text)
+
+        val sonuclar = mutableListOf<SearchResponse>()
+
+        veriler?.channels?.forEach { item ->
+            val toDict = jacksonObjectMapper().writeValueAsString(item)
+
+            sonuclar.add(newMovieSearchResponse(item.title, "${toDict}", TvType.Movie) { this.posterUrl = item.image })
+        }
+
+        veriler?.posters?.forEach { item ->
+            val toDict = jacksonObjectMapper().writeValueAsString(item)
+
+            sonuclar.add(newMovieSearchResponse(item.title, "${toDict}", TvType.Movie) { this.posterUrl = item.image })
+        }
+
+        return sonuclar
     }
 
     override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
@@ -77,38 +102,4 @@ class RecTV : MainAPI() {
 
         return true
     }
-
-    data class RecItem(
-        @JsonProperty("id")          val id: Int,
-        @JsonProperty("type")        val type: String?,
-        @JsonProperty("title")       val title: String,
-        @JsonProperty("label")       val label: String?,
-        @JsonProperty("sublabel")    val sublabel: String?,
-        @JsonProperty("description") val description: String?,
-        @JsonProperty("year")        val year: Int?,
-        @JsonProperty("imdb")        val imdb: Int?,
-        @JsonProperty("rating")      val rating: Float?,
-        @JsonProperty("duration")    val duration: String?,
-        @JsonProperty("image")       val image: String,
-        @JsonProperty("genres")      val genres: List<Genre>?,
-        @JsonProperty("trailer")     val trailer: Trailer?,
-        @JsonProperty("sources")     val sources: List<Source>
-    )
-
-    data class Genre(
-        @JsonProperty("id")    val id: Int,
-        @JsonProperty("title") val title: String
-    )
-
-    data class Trailer(
-        @JsonProperty("id")    val id: Int,
-        @JsonProperty("type")  val type: String,
-        @JsonProperty("url")   val url: String
-    )
-
-    data class Source(
-        @JsonProperty("id")    val id:Int,
-        @JsonProperty("type")  val type:String,
-        @JsonProperty("url")   val url:String
-    )
 }
